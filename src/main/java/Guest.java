e870d9ba7ccefcd313d3b3d69cfed4410e9c0e4f
jhtdod <jess.dodge@hotmail.co.uk>
2022-02-10 10:33:59 +0000

    guest setup complete and getName and setName tests passed

--- src/main/java/Guest.java
+++ src/main/java/Guest.java
@@ -5,5 +5,12 @@ public class Guest {
     public Guest(String name){
         this.name = name;
     }
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/test/java/GuestTest.java
+++ src/test/java/GuestTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+    Guest guest;
+
+    @Before
+    public void before(){
+        guest = new Guest("Sheila");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Sheila", guest.getName());
+    }
+
+    @Test
+    public void canSetName(){
+        guest.setName("Sheila Barrymore");
+        assertEquals("Sheila Barrymore", guest.getName());
+    }
+
 }
